fae1337e883642d9cc491b634e84d0269c762b28
Thierry Supplisson <ts@js353.com>
2022-12-13 17:27:01 +0000

    reduce size of mem.com

--- mem.asm
+++ mem.asm
@@ -9,16 +9,15 @@ _start:
             ; Get BIOS Configuration
             int     12h
             push    ax
-            push    ax
             jc      short exit
             mov     cl, 6
-            mov     ax, cs
-            shr     ax, cl
-            inc     ax
-            mov     bx, ax
-            pop     ax
+            mov     bx, cs
+            shr     bx, cl
+            inc     bx
             sub     ax, bx
 
+            ; Convert to string, starting with the last digit
+            ; Overwrites code we don't need any more, to save space
             jmp     short display
 digitend:   
             db "K$"
@@ -33,12 +32,12 @@ display:
             call    convert
 exit:
             mov     ah, 9
-            lea     dx, [eol]
+            mov     dx, offset eol
             int     21h
             int     20h
 convert:
             mov     bl, 10
-            lea     di, [digitend]
+            mov     di, offset digitend
 cvtloop:
             dec     di
             div     bl
